d856515b18f155189842b2584002d8dab6041536
Faiz Ahamed <76490386+FaizAhamed2o1@users.noreply.github.com>
2025-05-25 01:21:48 +0600

    Feature: Hero and footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Footer from "./Footer";
 import Header from "./Header";
+import HeroSection from "./HeroSection";
 
 function App() {
   return (
     <>
       <Header />
+
+      <HeroSection />
+
+      <Footer />
     </>
   );
 }
